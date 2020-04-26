8033a80c8688ccdd8a5986ec0f5a4616af7c64fd
Fabien SOLDE <fabien.solde@epfedu.fr>
2020-04-26 10:39:51 +0200

    Test

--- ananas/login/EmailBackend.py
+++ ananas/login/EmailBackend.py
@@ -6,12 +6,8 @@ class EmailBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-<<<<<<< HEAD
             email = kwargs['email']
-=======
-            email=kwargs['email']
             print(email)
->>>>>>> c6d4e4d931dfa6349270095376d9401c94bc5fa8
             user = UserModel.objects.get(email=email)
         except UserModel.DoesNotExist:
             return None
