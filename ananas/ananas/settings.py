d4fdb87ee133d389f10c3b82fe6fc61fc7995634
paul de boissel <paul.deboissel@epfedu.fr>
2020-05-01 11:00:18 +0200

    commit paul

--- ananas/ananas/settings.py
+++ ananas/ananas/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'login',
     'messenger',
     'phonenumber_field',
