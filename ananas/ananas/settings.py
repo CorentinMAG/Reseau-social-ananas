a9cb4eb3fc1cc8af91380dd0e8cb93c1725c2121
corentin <corentin.magyar@epfedu.fr>
2020-05-01 16:29:19 +0200

    timeline admin + on a remis channels dans settings.py

--- ananas/ananas/settings.py
+++ ananas/ananas/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
+    'channels',
     'messenger',
     'timeline',
     'phonenumber_field',
--- ananas/timeline/admin.py
+++ ananas/timeline/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
-#from . model import article
-#admin. site .register model
+from django.utils.text import Truncator
+from .models import Article
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    Personnalise l'affichage des articles dans l'interface admin
+    """
+    list_display = ('titre', 'auteur', 'date', 'apercu_contenu')
+    list_filter = ('auteur',)
+    date_hierarchy = 'date'
+    ordering = ('date',)
+    search_fields = ('titre',)
+    def apercu_contenu(self, article):
+        """
+        Retourne les 40 premiers caractères du contenu de l'article,
+        suivi de points de suspension si le texte est plus long.
+        """
+        return Truncator(article.contenu_post).chars(40, truncate='...')
+# Ajoute la fonctionnalité ajouter un article dans http://localhost:8000/admin/
+admin.site.register(Article, ArticleAdmin)
