0ab84fe03c913ad842cf42b8696ea5df238b65d5
corentin <corentin.magyar@epfedu.fr>
2020-05-05 18:50:04 +0200

    on ne coche pas le bouton rester connecté de base

--- ananas/login/forms.py
+++ ananas/login/forms.py
@@ -55,7 +55,7 @@ class ConnexionForm(forms.Form):
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'id': 'email'}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Mot de passe', 'id': 'password'}))
-    stay_connected = forms.BooleanField(required=False, initial=True, widget=forms.CheckboxInput(
+    stay_connected = forms.BooleanField(initial=False, widget=forms.CheckboxInput(
         attrs={'class': 'onoffswitch-checkbox', 'id': 'myonoffswitch'}))
 
     def clean_email(self):
