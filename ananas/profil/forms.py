eaf7c5db3b9011a6540b8c756d4fc8fa41bf0272
Margot Jourdan <margot.jourdan@epfedu.fr>
2020-06-01 16:21:30 +0200

    redimensionnement photo

--- ananas/profil/forms.py
+++ ananas/profil/forms.py
@@ -5,6 +5,9 @@ from string import Template
 from django.contrib.auth import get_user_model
 import re
 from django.utils.safestring import mark_safe
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
 
 User = get_user_model()
 
@@ -16,24 +19,41 @@ class ProfilForm(forms.Form):
                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom', 'id': 'nom'}))
     prenom = forms.CharField(label="", widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Prénom', 'id': 'prenom'}))
-    naissance = forms.CharField(max_length=10,required=False,label="", widget=forms.TextInput(
+    naissance = forms.CharField(max_length=10, required=False, label="", widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'jj/mm/aaaa', 'id': 'naissance'}))
-    password1 = forms.CharField(required=False,label="", widget=forms.PasswordInput(
+    password1 = forms.CharField(required=False, label="", widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Mot de passe', 'id': 'password1'}))
-    password2 = forms.CharField(required=False,label="", widget=forms.PasswordInput(
+    password2 = forms.CharField(required=False, label="", widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Vérification du mot de passe', 'id': 'password2'}))
-    majeure = forms.ModelChoiceField(required=False,queryset=Majeure.objects.all(), label="",
+    majeure = forms.ModelChoiceField(required=False, queryset=Majeure.objects.all(), label="",
                                      widget=forms.Select(attrs={'class': 'form-control', 'id': 'majeure'}))
-    campus = forms.ModelChoiceField(required=False,queryset=Campus.objects.all(), label="",
+    campus = forms.ModelChoiceField(required=False, queryset=Campus.objects.all(), label="",
                                     widget=forms.Select(attrs={'class': 'form-control', 'id': 'campus'}))
-    poste = forms.CharField(required=False,widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'poste','placeholder':'Entrer votre poste...'}))
+    poste = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'id': 'poste', 'placeholder': 'Entrer votre poste...'}))
 
     def clean_naissance(self):
         naissance = self.cleaned_data['naissance']
         regex = re.compile('^[0-9]{2}/[0-9]{2}/[0-9]{4}$')
-        if naissance =="":
+        if naissance == "":
             return naissance
         elif not regex.match(naissance):
             raise forms.ValidationError('Mauvais Format')
         return naissance
 
+    def clean_photo(self):
+
+        """Makes thumbnails of given size from given image"""
+        image = self.cleaned_data['photo']
+
+        im = Image.open(image)
+        size = 150, 150
+        im.thumbnail(size)  # resize image
+
+        thumb_io = BytesIO()  # create a BytesIO object
+
+        im.save(thumb_io, 'JPEG', quality=100)  # save image to BytesIO object
+
+        photo = File(thumb_io, name=image.name)  # create a django friendly File object
+
+        return photo
