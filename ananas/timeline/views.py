eb2967174ec0322b9fdd49bfdf5a5de0095582c3
corentin <corentin.magyar@epfedu.fr>
2020-06-06 23:17:21 +0200

    quelques chgt, commentaires avec une version unique de bootstrap

--- ananas/timeline/views.py
+++ ananas/timeline/views.py
@@ -25,7 +25,7 @@ def timeline(request):
         can_add_article = request.user.has_perm('timeline.add_article')
         args = {'posts': posts, 'form': form, 'can_add_article': can_add_article,
                 'username': mark_safe(json.dumps(request.user.first_name)),
-                'tags':tags,
+                'tags': tags,
                 'email': mark_safe(json.dumps(request.user.email))}
         return render(request, 'timeline/timeline.html', args)
     elif request.method == 'POST':
@@ -49,18 +49,19 @@ def search(request, int):
     args = {'posts': posts,
             'can_add_article': can_add_article,
             'form': form,
-            'tags':tags,
+            'tags': tags,
             'username': mark_safe(json.dumps(request.user.first_name)),
             'email': mark_safe(json.dumps(request.user.email))
             }
     return render(request, 'timeline/timeline.html', args)
 
-def searchType(request,type_tag):
+
+def searchType(request, type_tag):
     """
     recherche en fonction d'un tag ==> Go timeline filtrée
     """
     tags = Tags.objects.filter(type_tag=type_tag).exclude(type_tag='invisible')
-    posts=[]
+    posts = []
     for tag in tags:
         articles = tag.tag.all()
         posts = list(chain(posts, articles))
@@ -71,11 +72,12 @@ def searchType(request,type_tag):
     args = {'posts': posts,
             'can_add_article': can_add_article,
             'form': form,
-            'tags':tags,
+            'tags': tags,
             'username': mark_safe(json.dumps(request.user.first_name)),
             'email': mark_safe(json.dumps(request.user.email))
             }
     return render(request, 'timeline/timeline.html', args)
+
 
 def delete_article(request, id):
     article = Article.objects.get(pk=id)
@@ -88,9 +90,10 @@ def delete_article(request, id):
 def delete_comm(request, id):
     comm = Commentaires.objects.get(pk=id)
     article = comm.id_post.pk
+    slug = comm.id_post.slug
     if comm.id_user == request.user:
         comm.delete()
-    return redirect(reverse('view_article', kwargs={'id': article}))
+    return redirect(reverse('view_article', kwargs={'id': article, 'slug': slug}))
 
 
 @login_required
@@ -152,33 +155,33 @@ def lire(request, id, slug):
     """
 
     tags = Tags.objects.exclude(text_tag='All').order_by('text_tag')[:6]
-    try:
-        post = Article.objects.get(id=id, slug=slug)
-        tagsArticle = post.tags.all()
-        comments = Commentaires.objects.filter(id_post=id)
-    except post.DoesNotExist:
-        raise Http404
+    post = Article.objects.get(id=id, slug=slug)
+    tagsArticle = post.tags.all()
+    comments = Commentaires.objects.filter(id_post=id, parent=None)
 
     # COMMENTAIRES
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        new_comment = form.cleaned_data['contenu_comm']
-        truc = request.user
-        parent_obj = None
-        try:
-            parent_id = int(request.POST.get('parent_id'))
-        except:
-            parent_id = None
-        if parent_id:
-            parent_qs = Commentaires.objects.filter(pk=parent_id)
-            if parent_qs.exists() and parent_qs.count() == 1:
-                parent_obj = parent_qs.first()
-        com = Commentaires.objects.create(contenu_comm=new_comment, id_post=post, id_user=truc, parent=parent_obj)
-        com.save()
-        comments = Commentaires.objects.filter(id_post=id, parent=None)  # Actualise liste commentaires
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.cleaned_data['contenu_comm']
+            truc = request.user
+            parent_obj = None
+            try:
+                parent_id = int(request.POST.get('parent_id'))
+            except:
+                parent_id = None
+            if parent_id:
+                parent_qs = Commentaires.objects.filter(pk=parent_id)
+                if parent_qs.exists() and parent_qs.count() == 1:
+                    parent_obj = parent_qs.first()
+            com = Commentaires.objects.create(contenu_comm=new_comment, id_post=post, id_user=truc, parent=parent_obj)
+            com.save()
+            comments = Commentaires.objects.filter(id_post=id, parent=None)  # Actualise liste commentaires
+            form = CommentForm()
+    else:
         form = CommentForm()
 
-    args = {'post': post, 'comments': comments, 'form': form,'tags':tags ,'tagsArticle': tagsArticle,
+    args = {'post': post, 'comments': comments, 'form': form, 'tags': tags, 'tagsArticle': tagsArticle,
             'username': mark_safe(json.dumps(request.user.first_name)),
             'email': mark_safe(json.dumps(request.user.email)), 'article': True}
     return render(request, 'timeline/lire.html', args)
